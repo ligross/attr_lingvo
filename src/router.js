a60dd1d20f3d39c60c99127a0e65fd0ae2bd77fc
Yulia Baranova <ligros7@gmail.com>
2020-06-10 23:06:08 +0300

    Remove autosize

--- src/router.js
+++ src/router.js
@@ -5,11 +5,9 @@ import Home from './views/Home.vue'
 import Api from './views/Api.vue'
 import 'vue-material/dist/vue-material.min.css'
 import 'vue-material/dist/theme/default.css'
-import TextareaAutosize from 'vue-textarea-autosize'
 
 Vue.use(Router)
 Vue.use(VueMaterial)
-Vue.use(TextareaAutosize)
 
 export default new Router({
   routes: [
